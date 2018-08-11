10cc38dbb214e8d72dd3b76c3c5bbbc96780d5e3
Ajar Vashisth <ajarv@hotmail.com>
2018-08-11 10:28:18 -0700

    stage1

--- web/app.py
+++ web/app.py
@@ -17,6 +17,21 @@ from models import *
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    return "Hello World!"
+
+@app.route('/env', methods=['GET'])
+def index():
+    rval = """
+    Headers
+    ---
+    {headers}
+    """.format(headers=request.headers)
+    
+    return rval
+
+
+@app.route('/bpost', methods=['GET', 'POST'])
+def bpost():
     if request.method == 'POST':
         text = request.form['text']
         post = Post(text)
@@ -24,6 +39,7 @@ def index():
         db.session.commit()
     posts = Post.query.order_by(Post.date_posted.desc()).all()
     return render_template('index.html', posts=posts)
+
 
 
 if __name__ == '__main__':
